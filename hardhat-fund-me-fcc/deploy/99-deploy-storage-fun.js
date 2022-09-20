3a055cbf941a19e72880d16dad42fa6d9c5b1321
JPLACLAU <laclaujeanpaul@gmail.com>
2022-09-20 15:55:30 -0300

    z

--- hardhat-fund-me-fcc/deploy/99-deploy-storage-fun.js
+++ hardhat-fund-me-fcc/deploy/99-deploy-storage-fun.js
@@ -15,8 +15,11 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         // we need to wait if on a live network so we can verify properly
         waitConfirmations: network.config.blockConfirmations || 1,
     })
-    
-    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
         await verify(funWithStorage.address, [])
     }
 
@@ -29,27 +32,6 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
             )}`
         )
     }
-
-    // You can use this to trace!
-    // const trace = await network.provider.send("debug_traceTransaction", [
-    //     funWithStorage.transactionHash,
-    // ])
-    // for (structLog in trace.structLogs) {
-    //     if (trace.structLogs[structLog].op == "SSTORE") {
-    //         console.log(trace.structLogs[structLog])
-    //     }
-    // }
-    // const firstelementLocation = ethers.utils.keccak256(
-    //     "0x0000000000000000000000000000000000000000000000000000000000000002"
-    // )
-    // const arrayElement = await ethers.provider.getStorageAt(
-    //     funWithStorage.address,
-    //     firstelementLocation
-    // )
-    // log(`Location ${firstelementLocation}: ${arrayElement}`)
-
-    // Can you write a function that finds the storage slot of the arrays and mappings?
-    // And then find the data in those slots?
 }
 
 module.exports.tags = ["storage"]
