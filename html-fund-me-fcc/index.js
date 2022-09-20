31eda97256cd7e64ac74f8d6828a2217c610807b
JPLACLAU <laclaujeanpaul@gmail.com>
2022-09-20 15:18:28 -0300

    z

--- html-fund-me-fcc/index.js
+++ html-fund-me-fcc/index.js
@@ -1,33 +1,91 @@
-console.log("hi from script tag") //this is javascript
-
 import { ethers } from "./ethers-5.6.esm.min.js"
-import { abi } from "./constants.js"
+import { abi, contractAddress } from "./constants.js"
 
 const connectButton = document.getElementById("connectButton")
+const withdrawButton = document.getElementById("withdrawButton")
 const fundButton = document.getElementById("fundButton")
+const balanceButton = document.getElementById("balanceButton")
 connectButton.onclick = connect
+withdrawButton.onclick = withdraw
 fundButton.onclick = fund
+balanceButton.onclick = getBalance
 
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
-        console.log("I see a metamask! Yupiii!")
-        await window.ethereum.request({ method: "eth_requestAccounts" })
-        connectButton.innerHTML = "Connected!"
+        try {
+            await ethereum.request({ method: "eth_requestAccounts" })
+        } catch (error) {
+            console.log(error)
+        }
+        connectButton.innerHTML = "Connected"
+        const accounts = await ethereum.request({ method: "eth_accounts" })
+        console.log(accounts)
     } else {
-        connectButton.innerHTML = "Please Install Metamask."
+        connectButton.innerHTML = "Please install MetaMask"
     }
 }
 
-async function fund(ethAmount) {
+async function withdraw() {
+    console.log(`Withdrawing...`)
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        await provider.send("eth_requestAccounts", [])
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.withdraw()
+            await listenForTransactionMine(transactionResponse, provider)
+            // await transactionResponse.wait(1)
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        withdrawButton.innerHTML = "Please install MetaMask"
+    }
+}
+
+async function fund() {
+    const ethAmount = document.getElementById("ethAmount").value
     console.log(`Funding with ${ethAmount}...`)
     if (typeof window.ethereum !== "undefined") {
         const provider = new ethers.providers.Web3Provider(window.ethereum)
         const signer = provider.getSigner()
-        console.log(signer)
-        const contract = "" // ?
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            })
+            await listenForTransactionMine(transactionResponse, provider)
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        fundButton.innerHTML = "Please install MetaMask"
     }
-    // provider/ conecction to blockchain,
-    //signer / wallet / someonewith gas
-    //contract that  that we are interacting
-    // abi , address
+}
+
+async function getBalance() {
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        try {
+            const balance = await provider.getBalance(contractAddress)
+            console.log(ethers.utils.formatEther(balance))
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        balanceButton.innerHTML = "Please install MetaMask"
+    }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}`)
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            console.log(
+                `Completed with ${transactionReceipt.confirmations} confirmations. `
+            )
+            resolve()
+        })
+    })
 }
