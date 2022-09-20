7a126d619251c122e33c6cad40e30fb3797d62ef
JPLACLAU <laclaujeanpaul@gmail.com>
2022-09-20 13:20:51 -0300

    z

--- html-fund-me-fcc/index.js
+++ html-fund-me-fcc/index.js
@@ -1,14 +1,22 @@
-console.log("hi from script tag"); //this is javascript
+console.log("hi from script tag") //this is javascript
 
 async function connect() {
-  if (typeof window.ethereum !== "undefined") {
-    console.log("I see a metamask! Yupiii!");
-    await window.ethereum.request({ method: "eth_requestAccounts" });
-    document.getElementById("connectButton").innerHTML = "Connected!";
-  } else {
-    document.getElementById("connectButton").innerHTML =
-      "Please Install Metamask.";
-  }
+    if (typeof window.ethereum !== "undefined") {
+        console.log("I see a metamask! Yupiii!")
+        await window.ethereum.request({ method: "eth_requestAccounts" })
+        document.getElementById("connectButton").innerHTML = "Connected!"
+    } else {
+        document.getElementById("connectButton").innerHTML =
+            "Please Install Metamask."
+    }
 }
 
-async function fund(ethAmount) {}
+async function fund(ethAmount) {
+    console.log("Funding with ${ethAmount}...")
+    if (typeof window.ethereum !== "undefined") {
+    }
+    // provider/ conecction to blockchain,
+    //signer / wallet / someonewith gas
+    //contract that  that we are interacting
+    // abi , address
+}
