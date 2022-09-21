68601bffdd4439d93c35326f6cab255cc4411475
JPLACLAU <laclaujeanpaul@gmail.com>
2022-09-21 12:33:56 -0300

    z

--- hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
+++ hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
@@ -1,14 +1,21 @@
 //SPDX-lLicense-Identifier: MIT
 pragma solidity ^0.8.17;
 
+error Raffle__NotEnoughETHEntered();
+
 contract Raffle {
     uint256 private immutable i_entranceFee;
+    address[] player;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() {}
+    function enterRaffle() {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+    }
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
