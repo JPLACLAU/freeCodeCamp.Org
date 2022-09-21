47dc2d9c11020861f46dadb1efa9ae05bcd7940d
JPLACLAU <laclaujeanpaul@gmail.com>
2022-09-21 12:47:08 -0300

    z

--- hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
+++ hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
@@ -5,23 +5,24 @@ error Raffle__NotEnoughETHEntered();
 
 contract Raffle {
     uint256 private immutable i_entranceFee;
-    address payable[] private s_player;
+    address payable[] private s_players;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() {
+    function enterRaffle() public payable {
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughETHEntered();
         }
+        s_players.push(payable(msg.sender));
     }
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
 
-    function getPlayer() public view returns (address) {
+    function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
     }
 }
