f1e4841e232eb6b5066d260443a376664a3dbcd0
JPLACLAU <laclaujeanpaul@gmail.com>
2022-09-23 14:01:23 -0300

    z

--- hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
+++ hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
@@ -1,15 +1,17 @@
 //SPDX-lLicense-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(address vrCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
@@ -21,7 +23,11 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
-    function puckRandomWinner() external {}
+    function requestRandomWinner() external {
+        // 2 transaction process
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internaloverride {}
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
