cbc9b6ed419e685255eb4538d60531ee5bc44646
JPLACLAU <laclaujeanpaul@gmail.com>
2022-09-23 16:29:19 -0300

    z

--- hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
+++ hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
@@ -5,6 +5,7 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Raffle__NotEnoughETHEntered();
+error Raffle__TransferFailed();
 
 contract Raffle is VRFConsumerBaseV2 {
     uint256 private immutable i_entranceFee;
@@ -14,15 +15,21 @@ contract Raffle is VRFConsumerBaseV2 {
     uint64 private immutable i_subscriptionId;
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
+
+    // lottery variables - brag that you won
+    address private s_recentWinner;
 
     event RaffleEnter(address indexed player);
+    event RequestedRaffleWinner(uint256 requestId);
+    event WinnerPicked(address indexed winner);
 
     constructor(
         address vrCoordinatorV2,
         uint256 entranceFee,
         bytes32 gasLane,
         uint64 subscriptionId,
-        uint32 callbackGasLimit,
+        uint32 callbackGasLimit
     ) VRFConsumerBaseV2(vrCoordinatorV2) {
         i_entranceFee = entranceFee;
         i_vrCoordinator = VRFCoordinatorV2Interface(vrCoordinatorV2);
@@ -41,19 +48,32 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function requestRandomWinner() external {
         // 2 transaction process
-        i_vrCoordinator.requestRandomWords(
+        uint256 requestId = i_vrCoordinator.requestRandomWords(
             i_gasLane, // gasLane
             i_subscriptionId,
             REQUEST_CONFIRMATIONS,
             i_callbackGasLimit,
-            numWords
+            NUM_WORDS
         );
+        emit RequestedRaffleWinner(requestId);
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
-        internal
-        override
-    {}
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        // s_player size 10, but randonumber is 200. if we 202 divided mod 10.
+        // 202 mod 10. will be 2. it is what does not fit into 202.
+        // % is mod  Modulo.
+
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        s_players = new address payable[](0);
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        // require success
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
+    }
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
@@ -61,5 +81,9 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
     }
 }
